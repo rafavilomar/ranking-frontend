331698eda7a11652772abf2272b3061c4e5f64be
rafavilomar <rafavilomar@gmial.com>
2021-11-06 22:49:37 -0400

    feat: add input to search box #1

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,37 +1,48 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import {SearchIcon} from "@heroicons/react/solid";
+import { SearchIcon } from "@heroicons/react/solid";
 
 const Header = () => {
   return (
     <header className="shadow-md bg-white py-2">
       <div className="container justify-between items-center inline-flex px-4">
-	{/*---BRAND NAME---*/}
-	<div> 
-	  <h1 className="font-sans"> RANKING </h1> 
-	</div>
-	
-	<div className="gap-5 w-24 md:w-auto lg:48 grid grid-cols-2 ">
-	  {/*--SEARCH BOX---*/} 
-	  <div className="W-24 w-full bg-blue-400">
-	    <div></div>
-	    <button className="flex items-center justify-center px-2 py-2 ">
-	      <SearchIcon className="h-5"/>
-	    </button>
-	  </div>
-	  <div className="flex items-center gap-5">
-	    {/*--PROFILE BUTTON--*/} 
-	    <button className="rounded-full w-12 min-w-12" > 
-	      <img className="rounded-full" src="https://cultivatedculture.com/wp-content/uploads/2019/12/LinkedIn-Profile-Picture-Example-Madeline-Mann-414x414.jpeg" /> 
-	    </button>
-	    {/*--ACTION BUTTONS--*/}
-	    <Link to="/" className="py-2 px-4 rounded-lg shadow-md bg-green-500 text-white"> Sign up </Link>
-	  </div>
-	</div>
+        {/*---BRAND NAME---*/}
+        <div>
+          <h1 className="font-sans"> RANKING </h1>
+        </div>
+
+        <div className="gap-5 w-full flex justify-end lg:48">
+          {/*--SEARCH BOX---*/}
+          <div className="max-w-md border-gray-400 w-full flex overflow-hidden rounded-md border px-2">
+            <div className="w-full h-full">
+							<input className="w-full h-full  border-none focus:shadow-none focus:outline-none px-2" type="text" name="search" id="search" placeholder="Buscar Profesor" />
+						</div>
+            <button className="flex items-center justify-center px-2 py-2 ">
+              <SearchIcon className="h-5" />
+            </button>
+          </div>
+          <div className="flex items-center gap-5">
+            {/*--PROFILE BUTTON--*/}
+            <button className="rounded-full w-12 min-w-12">
+              <img
+                className="rounded-full"
+                src="https://cultivatedculture.com/wp-content/uploads/2019/12/LinkedIn-Profile-Picture-Example-Madeline-Mann-414x414.jpeg"
+              />
+            </button>
+            {/*--ACTION BUTTONS--*/}
+            <Link
+              to="/"
+              className="py-2 px-4 rounded-lg shadow-md bg-green-500 text-white"
+            >
+              {" "}
+              Sign up{" "}
+            </Link>
+          </div>
+        </div>
       </div>
     </header>
   );
-}
+};
 
 export default Header;
