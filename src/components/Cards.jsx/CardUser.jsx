98d7b1cb41bc6c17a2269e96b8ad1d670cd4476d
rafavilomar <rafavilomar@gmial.com>
2021-11-11 23:10:00 -0400

    feat: top rated #7

---
+++ src/components/Cards.jsx/CardUser.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const CardUser = ({ type = "NORMAL", image, title, content, rate, color }) => {
+  const [className, setClassName] = React.useState();
+
+  React.useEffect(
+    (rule) => {
+      if (type === "NORMAL") {
+        setClassName(" w-80 h-96");
+      } else {
+        setClassName("w-64 h-72");
+      }
+    },
+    [type]
+  );
+
+  return (
+    <div
+      className={`${className} mt-5 shadow-md relative flex items-center justify-center`}
+    >
+      <span
+        className={`${color} font-sans text-2xl font-semibold absolute flex justify-center items-center -top-7 left-auto right-auto bottom-auto rounded-br-full rounded-bl-full bg-red-400 w-14 h-14`}
+      >
+        {rate}
+      </span>
+      <div className="overflow-hidden max-h-full rounded-xl max-w-full flex items-center justify-center">
+        <img src={image} alt="noe" />
+        {type === "NORMAL" && (
+          <div className="absolute bg-white bottom-0 p-4 left-0 right-0 top-auto">
+            <p className="font-sans">300 valoraciones</p>
+          </div>
+        )}
+        {/* <div className="absolute bg-white bottom-0 p-4 left-0 right-0 top-auto">
+        <h6 className="font-sans font-semibold text-lg">Nombre del profesor</h6>
+        <p className="font-sans">Asignatura</p>
+      </div> */}
+      </div>
+    </div>
+  );
+};
+export default CardUser;
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,29 +1,41 @@
 import React from "react";
+import CardUser from "../components/Cards.jsx/CardUser";
 
 import Header from "../components/layout/Header";
 
 const Home = () => {
+
   return (
     <div>
       <Header />
       <div className="max-w-7xl mx-auto">
         <div className="flex justify-center">
           <div>
-            <h3 className="font-sans font-semibold text-center text-3xl">
-              ¿Qué piensas de este profesor? 🤔
-            </h3>
             <div className="mt-5 w-96 h-96 overflow-hidden shadow-md flex items-center justify-center rounded-xl relative">
               <img
                 src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
                 alt="noe"
               />
-              <div className="absolute bg-gray-200 bottom-0 p-4 left-0 right-0 top-auto">
-                <h6 className="font-sans font-semibold text-lg">Nombre del profesor</h6>
+              <div className="absolute bg-white bottom-0 p-4 left-0 right-0 top-auto">
+                <h6 className="font-sans font-semibold text-lg">
+                  Nombre del profesor
+                </h6>
                 <p className="font-sans">Asignatura</p>
               </div>
             </div>
           </div>
         </div>
+      </div>
+      <div className="mt-48">
+            <h3 className="font-sans font-semibold text-center text-3xl">
+            Conoce a los mejores
+            </h3>
+      </div>
+      <div className="mt-3 max-w-7xl mx-auto flex justify-center gap-5 items-center">
+
+        <CardUser color="bg-gray-400" rate={2} type="SMALL" image="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+        <CardUser color="bg-yellow-400" rate={1} image="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+        <CardUser color="bg-yellow-700" rate={3} type="SMALL" image="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
       </div>
     </div>
   );
