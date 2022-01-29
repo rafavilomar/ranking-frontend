450a71a4c3e9023766a5e297446d0cec38c1c021
rafavilomar <rafavilomar@gmail.com>
2022-01-28 20:46:49 -0400

    handle token on DataService #18

--- src/fetcher/DataService.js
+++ src/fetcher/DataService.js
@@ -2,12 +2,14 @@ import axios from "axios";
 
 class DataService {
   async get(url, headers) {
-    const result = await axios.get(url);
+    const token = `Bearer ${localStorage.getItem("token")}`;
+    const result = await axios.get(url, { headers: { Authorization: token, ...headers } });
     return result.data;
   }
 
   async post(url, data, headers) {
-    const result = await axios.post(url, data);
+    const token = `Bearer ${localStorage.getItem("token")}`;
+    const result = await axios.post(url, data, { headers: { Authorization: token, ...headers } });
     return result.data;
   }
 }
